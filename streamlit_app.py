c82a04a7430f422476d231630c8bf3aed3d3f6ba
Derek (Yue) Yu <derekyuyue@gmail.com>
2025-03-20 21:42:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,120 @@
 import streamlit as st
 from openai import OpenAI
+import json
+import os
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+DB_FILE = 'db.json'
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+def main():
+    client = OpenAI(api_key=st.session_state.openai_api_key)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    # List of models
+    models = ["gpt-4o-mini", "gpt-4o", "gpt-4-turbo", "gpt-4", "gpt-3.5-turbo"]
 
-    # Display the existing chat messages via `st.chat_message`.
+    # Create a select box for the models
+    st.session_state["openai_model"] = st.sidebar.selectbox("Select OpenAI model", models, index=0)
+    st.session_state["tone"] = st.sidebar.selectbox("Customize my tone!", ('Normal', 'Philosophical', 'OUTRAGEOUSLY FUNNY!', 'Depressed :('))
+    st.session_state["lang"] = st.sidebar.selectbox("Language", ('English', 'Chinese (Mandarin)', 'Korean', 'French'))
+    st.header("Unless Bot!")
+    st.subheader(":green[...unless?]")
+    # Load chat history from db.json
+    with open(DB_FILE, 'r') as file:
+        db = json.load(file)
+    st.session_state.messages = db.get('chat_history', [])
+
+    # Display chat messages from history on app rerun
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
+    # Accept user input
+    if prompt := st.chat_input("Tell me a fact! (ex. I will eat tacos every day)"):
+        # Add user message to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
+        # Display assistant response in chat message container
         with st.chat_message("assistant"):
+            tone=st.session_state["tone"]
+            lang=st.session_state["lang"]
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"] + ". If this is falsifiable, make a(n) " + str(tone) + " response within 15 words in " + str(lang) + "and start your answer with the word '...unless'. Else, respond normally."}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
+
+        # Store chat history to db.json
+        db['chat_history'] = st.session_state.messages
+        with open(DB_FILE, 'w') as file:
+            json.dump(db, file)
+
+    # Add a "Clear Chat" button to the sidebar
+    if st.sidebar.button('Clear Chat'):
+        # Clear chat history in db.json
+        db['chat_history'] = []
+        with open(DB_FILE, 'w') as file:
+            json.dump(db, file)
+        # Clear chat messages in session state
+        st.session_state.messages = []
+        st.rerun()
+
+
+if __name__ == '__main__':
+
+    if 'openai_api_key' in st.session_state and st.session_state.openai_api_key:
+        main()
+    
+    else:
+
+        # if the DB_FILE not exists, create it
+        if not os.path.exists(DB_FILE):
+            with open(DB_FILE, 'w') as file:
+                db = {
+                    'openai_api_keys': [],
+                    'chat_history': []
+                }
+                json.dump(db, file)
+        # load the database
+        else:
+            with open(DB_FILE, 'r') as file:
+                db = json.load(file)
+
+        # display the selectbox from db['openai_api_keys']
+        selected_key = st.selectbox(
+            label = "Existing OpenAI API Keys", 
+            options = db['openai_api_keys']
+        )
+
+        # a text input box for entering a new key
+        new_key = st.text_input(
+            label="New OpenAI API Key", 
+            type="password"
+        )
+
+        login = st.button("Login")
+
+        # if new_key is given, add it to db['openai_api_keys']
+        # if new_key is not given, use the selected_key
+        if login:
+            if new_key:
+                db['openai_api_keys'].append(new_key)
+                with open(DB_FILE, 'w') as file:
+                    json.dump(db, file)
+                st.success("Key saved successfully.")
+                st.session_state['openai_api_key'] = new_key
+                st.rerun()
+            else:
+                if selected_key:
+                    st.success(f"Logged in with key '{selected_key}'")
+                    st.session_state['openai_api_key'] = selected_key
+                    st.rerun()
+                else:
+                    st.error("API Key is required to login")
